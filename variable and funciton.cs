dce863e114a8bfee97a5b6ad97d1c83275e619d7
submitting variables and funcitons <art_is_life23@outlook.com>
2016-01-22 11:17:42 -0700

    submit va and fun

---
+++ variable and funciton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	int MyInt = 20;
+
+	void Start () {
+		MyInt = MultiplyByTwo (MyInt);
+		Debug.Log (MyInt);
+	}
+
+	int MultiplyByTwo(int number){
+		int kitty;
+
+		kitty = 2 * number;
+
+		return kitty;
+	}
+
+}
