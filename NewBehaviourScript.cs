aa74e3e8f4b710df4d9ac37e3d911eddaccf3b5d
Aubrey <art_is_life23@outlook.com>
2016-03-25 21:10:44 -0600

    opened final project decided to make it a bouncing bowling game. Created bowling ball, assigned rigid body, and gave physics material bounce to ball

---
+++ NewBehaviourScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal.GetAxis =("Horizontal");
+		float moveVeritcal.GetAxis = ("Vertical");
+
+		Rigidbody
+//check every frame for apply 
+}
